d3fc3c2e806aa3036b6a794486173d2272ae7b04
Earl P <epemberton@treadsec.com>
2025-06-14 21:12:26 -0500

    feat: update startup script to allow the install of snap/cerbot, and redirect mgmt subnet rt  to loadbalancer instead of nge

--- ssl_loadbalancer_build/ashburn/builds/certbot/startup.sh
+++ ssl_loadbalancer_build/ashburn/builds/certbot/startup.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-# Install snap
-sudo yum install snap
+# Add EPEL repo
+sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+sudo dnf upgrade -y
+
+# Install snap service
+sudo dnf install -y snapd
+
+# Enable and start snap service
+sudo systemctl enable --now snapd.socket
+sudo systemctl start snapd
+
+# Create symbolic link for snap
+sudo ln -s /var/lib/snapd/snap /snap
+
+# Install and refresh core service
+sudo snap install core
+sudo snap refresh core
 
 # Install Certbot
 sudo snap install --classic certbot
--- ssl_loadbalancer_build/ashburn/builds/network/main.tf
+++ ssl_loadbalancer_build/ashburn/builds/network/main.tf
@@ -34,7 +34,7 @@ resource "oci_core_route_table" "mgmt_subnet_rt" {
     display_name = "Default Route Table for mgmt-subnet"
 
     route_rules {
-        network_entity_id = oci_core_nat_gateway.hub_ngw.id
+        network_entity_id = oci_load_balancer_load_balancer.pub_lb.id
         description = "Forwards packets in mgmt subnet to ngw."
         destination = "0.0.0.0/0"
         destination_type = "CIDR_BLOCK"
